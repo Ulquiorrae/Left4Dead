357d932086928dd95cd50f34dfbad2e0a9b47874
Gordan Sajević <gsajevic@hotmail.com>
2014-12-18 15:15:14 +0100

    Update Zombie.java

--- Left4Dead/src/Zombie.java
+++ Left4Dead/src/Zombie.java
@@ -1,4 +1,21 @@
+public class Zombie extends Character {
 
-public class Zombie {
+	//Selma
+	
+public Zombie(String name,double HP, double Dexterity, Weapons weapon, double strength,double speed){
+super(name, HP, Dexterity, weapon, strength, speed);
+}
+
+@Override
+public String toString() {
+
+	return super.toString();
+}
+
+@Override
+public boolean equals(Object obj) {
+
+	return super.equals(obj);
+}
 
 }
