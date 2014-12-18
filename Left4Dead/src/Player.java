c61c849903181f8774f6b42ac9fb2e1bcd01863a
Gordan Sajević <gsajevic@hotmail.com>
2014-12-18 15:15:59 +0100

    Update Player.java

--- Left4Dead/src/Player.java
+++ Left4Dead/src/Player.java
@@ -5,21 +5,49 @@ public class Player extends Character {
 	
 	private double intel;
 	
-	public Player(String name, double HP, double dexterity, Weapons weapon, double strength, double speed)
+	public Player(String name, double HP, double dexterity, Weapons weapon, double strength, double speed, double intel)
 	{	
-		super(name, HP, dexterity, weapon, strength, speed);
-		
-		
+		this.name = name;
+		this.HP = HP;
+		this.dexterity = dexterity;
+		this.weapon = weapon;
+		this.strenght = strength;
+		this.speed = speed;
+		this.intel = intel;
+	}
+	
+	public void setIntel(double intel) {
+		double temp;
+		temp = 0.5 + Math.random() * (2-0.5);
+		this.intel = temp;
 	}
 	
 
-	public double escapeChance()
+	@Override
+	public String toString() {
+		String temp="Intel: "+intel;
+		return super.toString()+temp;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+	
+		return super.equals(obj);
+	}
+
+	@Override
+	public double Attack() {
+		return super.Attack() * this.intel;
+	}
+	
+	public boolean escChance(Character z)
 	{
-		return intel;
+		int escape = (int) ((this.intel*this.speed)/z.speed);
+		if (escape == 0)
+		{
+			return false;
+		}
+		return true;
 	}
-
-	
-	
-	
 	
 }
